26d0f57ee53533b5c42fbd310cec7f8f51eb3662
are305 <are305@ensign.edu>
2023-10-21 19:24:27 -0600

    AE 10/21

--- wubrg_app/lib/randomCards.dart
+++ wubrg_app/lib/randomCards.dart
@@ -4,8 +4,23 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
   runApp(MyApp());
+
+  testWidgets('Testing if it generates 15 random cards correctly',
+      (WidgetTester tester) async {
+    //Correct 15 random cards:
+    // 1 x Multiverse Legend
+    // 1 x Double-Faced Battle
+    // 1 x Single-Faced
+    // 1 x Double-Faced
+    // 2 x Uncommons
+    // 8 x Commons
+    // 1 x Land (Basic Land or Dual Land)
+    //expect(find(Deck), matcher)
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -39,6 +54,26 @@ class _CardViewerState extends State<CardViewer> {
   }
 
   Future<void> loadCardAssets() async {
+    // final manifestContent = await rootBundle.loadString('AssetManifest.json');
+    // final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+
+    // final assetPaths = manifestMap.keys
+    //     .where((String key) => key.startsWith('assets/image'))
+    //     .toList();
+
+    // final Random random = Random();
+
+    // final List<String> newCardAssets = [];
+
+    // while (newCardAssets.length < numberOfCardsToShow) {
+    //   final randomIndex = random.nextInt(assetPaths.length);
+    //   final randomAsset = assetPaths[randomIndex];
+    //   newCardAssets.add(randomAsset);
+    // }
+
+    // setState(() {
+    //   cardAssets = newCardAssets;
+    // });
     final manifestContent = await rootBundle.loadString('AssetManifest.json');
     final Map<String, dynamic> manifestMap = json.decode(manifestContent);
 
@@ -50,10 +85,35 @@ class _CardViewerState extends State<CardViewer> {
 
     final List<String> newCardAssets = [];
 
-    while (newCardAssets.length < numberOfCardsToShow) {
-      final randomIndex = random.nextInt(assetPaths.length);
-      final randomAsset = assetPaths[randomIndex];
-      newCardAssets.add(randomAsset);
+    // Define the criteria
+    final criteria = {
+      'Multiverse Legend': 1,
+      'Double-Faced Battle': 1,
+      'Single-Faced': 1,
+      'Double-Faced': 1,
+      'Uncommon': 2,
+      'Common': 8,
+      'Land': 1,
+    };
+
+    for (final entry in criteria.entries) {
+      final cardName = entry.key;
+      final count = entry.value;
+      final eligibleCards = List<String>.from(
+          assetPaths.where((assetPath) => assetPath.contains(cardName)));
+
+      // Ensure there are enough eligible cards to meet the count requirement
+      if (eligibleCards.length < count) {
+        throw Exception('Not enough cards for: $cardName');
+      }
+
+      for (var i = 0; i < count; i++) {
+        final randomIndex = random.nextInt(eligibleCards.length);
+        final randomAsset = eligibleCards[randomIndex];
+        newCardAssets.add(randomAsset);
+        // Remove the selected card from eligibleCards to avoid duplicates
+        eligibleCards.removeAt(randomIndex);
+      }
     }
 
     setState(() {
