9720452944804431cb0c87b676ac5d55f2147ea4
WoodyLam1110 <121983241+WoodyLam1110@users.noreply.github.com>
2023-11-06 15:11:06 -0700

    bug fixed

--- wubrg_app/lib/screens/gamePhrase.dart
+++ wubrg_app/lib/screens/gamePhrase.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../services/ImageManager.dart';
 
 void main() {
@@ -7,75 +6,106 @@ void main() {
 }
 
 class GamePhrase extends StatelessWidget {
-  const GamePhrase({super.key});
+  const GamePhrase({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Wubrg Slider'),
-        ),
-        body: const Center(
-          child: ImageSlider(),
-        ),
+        body: ImageSlider(),
       ),
     );
   }
 }
 
 class ImageSlider extends StatefulWidget {
-  const ImageSlider({super.key});
+  const ImageSlider({Key? key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _ImageSliderState createState() => _ImageSliderState();
 }
 
 class _ImageSliderState extends State<ImageSlider> {
-  final ImageManager _imageManager = ImageManager(); // Create an instance
-
+  final ImageManager _imageManager = ImageManager();
   List<String> images = [];
   int currentIndex = 0;
+  List<String> selectedImages = [];
+  int cardTotal = 15; // Initialize cardTotal with the total number of cards
 
   @override
   void initState() {
     super.initState();
-    // Get a list of random images from the ImageManager
-    images = _imageManager.randomImages;
+    List<String> allImages = _imageManager.randomImages;
+    images =
+        allImages.take(cardTotal).toList(); // Use the initial cardTotal value
   }
 
-  //duplicate function
   void _nextImage() {
     setState(() {
-      // Remove the currently displayed image from the list
+      if (images.length > 1) {
+        images.removeAt(0);
+      }
+    });
+  }
+
+  void _selectCard() {
+    setState(() {
+      // Add the currently displayed image to the selectedImages list
+      selectedImages.add(images[currentIndex]);
+
+      // Remove the selected image from the images list
       images.removeAt(currentIndex);
 
-      // Check if all images have been displayed, and if so, reset the list
-      if (images.isEmpty) {
-        images = _imageManager.randomImages;
-        currentIndex = 0;
-      } else {
-        // Make sure currentIndex is within bounds
-        currentIndex = currentIndex % images.length;
-      }
+      // Decrement cardTotal and use the decremented value
+      cardTotal--;
+      images = _imageManager.randomImages.take(cardTotal).toList();
+      currentIndex = 0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 600, // Set the height of your slider as per your requirements
-      child: PageView.builder(
-        itemCount: images.length,
-        itemBuilder: (context, index) {
-          return Image.asset(images[index]);
-        },
-        onPageChanged: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (selectedImages.isNotEmpty)
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: selectedImages.length,
+                itemBuilder: (context, index) {
+                  return Image.asset(selectedImages[index]);
+                },
+              ),
+            ),
+          SizedBox(
+            height: 400,
+            child: PageView.builder(
+              itemCount: images.length,
+              itemBuilder: (context, index) {
+                return Image.asset(images[index]);
+              },
+              onPageChanged: (index) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _selectCard,
+            child: Text('Select Card'),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft, // Align the text to the left
+            child: Text(
+              'Total Cards: $cardTotal', // Use cardTotal here
+              style: TextStyle(fontSize: 16),
+            ),
+          )
+        ],
       ),
     );
   }
