545cd430dfba23c9c7e980ca7d10e0a9e4b7c6e8
are305 <are305@ensign.edu>
2023-12-09 17:37:01 -0700

    AE 12/9 EndGamePhrase

--- wubrg_app/lib/screens/EndGamePhrasse.dart
+++ wubrg_app/lib/screens/EndGamePhrasse.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wubrg_app/screens/gamePhrase.dart';
+import 'package:share/share.dart';
+import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const EndGame());
@@ -31,8 +34,75 @@ class EndGamePhrase extends StatefulWidget {
 }
 
 class _EndGamePhrase extends State<EndGamePhrase> {
+  // Function to share the game result
+  void _shareOnSocialMedia(int score) async {
+    String currentDate = DateFormat.yMd().format(DateTime.now());
+    String message = "Today $currentDate I scored $score on WUBRG Drafter";
+
+    // Share on Facebook
+    Share.share(message, subject: 'WUBRG Drafter Score');
+
+    // // Share on Twitter
+    // String twitterUrl = 'https://twitter.com/intent/tweet?text=${Uri.encodeComponent(message)}';
+    // if (await canLaunch(twitterUrl)) {
+    //   await launch(twitterUrl);
+    // } else {
+    //   print('Could not launch Twitter');
+    // }
+
+    // // Share on Instagram
+    // String instagramUrl = 'https://www.instagram.com/create/story/?text=${Uri.encodeComponent(message)}';
+    // if (await canLaunch(instagramUrl)) {
+    //   await launch(instagramUrl);
+    // } else {
+    //   print('Could not launch Instagram');
+    // }
+  }
+
+  // Function to share the game result
+  void _shareOnTwitter(int score) async {
+    String currentDate = DateFormat.yMd().format(DateTime.now());
+    String message = "Today $currentDate I scored $score on WUBRG Drafter";
+
+    // String twitterUrl = 'https://twitter.com/intent/tweet?text=${Uri.encodeComponent(message)}';
+    // if (await canLaunch(twitterUrl)) {
+    //   await launch(twitterUrl);
+    // } else {
+    //   print('Could not launch Twitter');
+    // }
+    Share.share('Twitter: $message', subject: 'WUBRG Drafter Score');
+  }
+
+  void _shareOnFacebook(int score) async {
+    String currentDate = DateFormat.yMd().format(DateTime.now());
+    String message = "Today $currentDate I scored $score on WUBRG Drafter";
+
+    // String facebookUrl = 'https://www.facebook.com/sharer/sharer.php?u=${Uri.encodeComponent(message)}';
+    // if (await canLaunch(facebookUrl)) {
+    //   await launch(facebookUrl);
+    // } else {
+    //   print('Could not launch Facebook');
+    // }
+    Share.share('Facebook: $message', subject: 'WUBRG Drafter Score');
+  }
+
+  void _shareOnInstagram(int score) async {
+    String currentDate = DateFormat.yMd().format(DateTime.now());
+    String message = "Today $currentDate I scored $score on WUBRG Drafter";
+
+    // String instagramUrl = 'https://www.instagram.com/create/story/?text=${Uri.encodeComponent(message)}';
+    // if (await canLaunch(instagramUrl)) {
+    //   await launch(instagramUrl);
+    // } else {
+    //   print('Could not launch Instagram');
+    // }
+    Share.share('Instagram: $message', subject: 'WUBRG Drafter Score');
+  }
+
   @override
   Widget build(BuildContext context) {
+    int finalScore = 97; // Replace with your actual final score
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
@@ -51,7 +121,7 @@ class _EndGamePhrase extends State<EndGamePhrase> {
                     style: TextStyle(fontSize: 24),
                   ),
                 ),
-                const SizedBox(height: 30), // Add a SizedBox for a bigger gap
+                const SizedBox(height: 30),
                 const Align(
                   alignment: Alignment.center,
                   child: Text(
@@ -66,6 +136,26 @@ class _EndGamePhrase extends State<EndGamePhrase> {
                     'Share on Social Media',
                     style: TextStyle(fontSize: 24),
                   ),
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => _shareOnTwitter(finalScore),
+                      child: const Text('Twitter'),
+                    ),
+                    const SizedBox(width: 10),
+                    ElevatedButton(
+                      onPressed: () => _shareOnFacebook(finalScore),
+                      child: const Text('Facebook'),
+                    ),
+                    const SizedBox(width: 10),
+                    ElevatedButton(
+                      onPressed: () => _shareOnInstagram(finalScore),
+                      child: const Text('Instagram'),
+                    ),
+                  ],
                 ),
                 const SizedBox(height: 30),
                 TextButton(
